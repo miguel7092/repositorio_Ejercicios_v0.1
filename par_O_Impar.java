dfbf9d89a45b6880426a846f61dea2d9ab71d888
miguel7092 <estecorreoesmicuentapublica@gmail.com>
2024-01-13 16:37:17 -0500

    Coloco un nuevo ejercicio utilizando if o else

---
+++ par_O_Impar.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class par_O_Impar {
+    public static void main(String[] args){
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Coloque el numero para conocer si es par o impar --> ");
+        int numero1 = teclado.nextInt();
+        int resto = numero1 % 2;
+        if (resto == 0){
+            System.out.println("es par");
+        }
+        else{
+            System.out.println("No es par");
+        }
+    }
+}
